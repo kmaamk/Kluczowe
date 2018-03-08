3d2b8220fef87704fdbe5f67f2df857a509a22cf
kmaamk <an.mk.poczta@gmail.com>
2018-03-08 11:58:05 +0100

    Signed-off-by: kmaamk <an.mk.poczta@gmail.com>

--- klucz/urls.py
+++ klucz/urls.py
@@ -6,9 +6,13 @@ from . import views
 
 app_name="klucz"
 urlpatterns =[
-    url(r'^$', views.wybierz_kluczowe, name='wybierz_kluczowe'),
-    url(r'^login/$', login, {'template_name': 'login2.html'}),
+    url(r'^$', views.login_view, name='logowanie'),
     url(r'potwierdzenie', views.potwierdzenie, name='potwierdzenie'),
+    url(r'klucz', views.wybierz_kluczowe, name='wybierz_kluczowe'),
+    url(r'^login/$', login, {'template_name': 'login2.html'}),
+
     url(r'^error/$', login, {'template_name': 'login_error.html'}),
 
 ]
+
+#url(r'^$', views.wybierz_kluczowe, name='wybierz_kluczowe'),
